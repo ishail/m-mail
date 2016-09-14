f92128ea1f6474200635b1cbb474aedbaaba8686
Shailesh <shailesh.iiita@gmail.com>
2016-09-14 17:17:33 +0530

    buildable source

--- message/message.go
+++ message/message.go
@@ -1,19 +1,1 @@
 package message
-
-import (
-	"bytes"
-	"m-mail/common"
-)
-
-func (m *Message) RFCString() ([]byte, error) {
-	var response bytes.Buffer
-	if _, ok := m.Header["Mime-Version"]; !ok {
-		response.WriteString("Mime-Version: 1.0\r\n")
-	}
-
-	if _, ok := m.Header["Date"]; !ok {
-		response.writeHeader("Date", m.FormatDate(now()))
-	}
-
-	return response.Bytes(), nil
-}
